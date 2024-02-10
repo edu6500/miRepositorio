5ab465b4f1a983a24faf74bb50e575022892c556
SCPC 111 <SCPC111@labs.upds.bo>
2024-02-09 21:37:55 -0400

    se agrego mi proyecto

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("ingrese primer numero");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("los numeros ingresados en orden ascendente sera:");
+            if (x > y)
+                Console.WriteLine(y + "," + x);
+            else
+                Console.WriteLine(x + "," + y);
+
+            Console.ReadKey();
+        }
+    }
+}
